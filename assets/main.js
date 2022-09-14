2fcd5092581d99fa2ed8caf461add094b959646f
Mounir habbas <Mounirawt@gmail.com>
2022-09-14 10:43:24 +0100

    v:0.0.5

--- assets/main.js
+++ assets/main.js
@@ -1,43 +1,43 @@
-// let creations__Gallery=document.querySelector(".creations__Gallery")
-// function getcards() {
+let creations__Gallery=document.querySelector(".creations__Gallery")
+function getcards() {
 
-//    let myReauest = new XMLHttpRequest();
+   let myReauest = new XMLHttpRequest();
 
-// myReauest.onreadystatechange=function () {
+myReauest.onreadystatechange=function () {
 
-//    if(myReauest.status===200 && myReauest.readyState===4){
-//                let cards= JSON.parse(this.responseText)
-//                let cardsCounter=cards.length
+   if(myReauest.status===200 && myReauest.readyState===4){
+               let cards= JSON.parse(this.responseText)
+               let cardsCounter=cards.length
                
 
-//    creations__Gallery.innerHTML=""
+   creations__Gallery.innerHTML=""
           
 
-//    for (let i= 0; i < cards.length; i++) {
+   for (let i= 0; i < cards.length; i++) {
       
-// let card =document.createElement("div") 
-//       card.className="card__Gallery"
-// console.log(cards[i].title)
-// card.innerHTML=`
-// <img class="desktop" src="./assets/images/desktop/${cards[i].src}.jpg" alt="">
-// <img class="mobile" src="./assets/images/mobile/${cards[i].src}.jpg" alt="">
-//   <div class="card__title"> ${cards[i].title} </div>
+let card =document.createElement("div") 
+      card.className="card__Gallery"
+console.log(cards[i].title)
+card.innerHTML=`
+<img class="desktop" src="./assets/images/desktop/${cards[i].src}.jpg" alt="">
+<img class="mobile" src="./assets/images/mobile/${cards[i].src}.jpg" alt="">
+  <div class="card__title"> ${cards[i].title} </div>
 
-// `
-//      creations__Gallery.appendChild(card) 
-//    }
+`
+     creations__Gallery.appendChild(card) 
+   }
 
 
    
    
    
-//    }
-// }
+   }
+}
 
-//  myReauest.open("GET","./assets/urcreations.json",true);
-//  myReauest.send();
-//  }
-//  getcards()
+ myReauest.open("GET","./assets/urcreations.json",true);
+ myReauest.send();
+ }
+ getcards()
 
 
 
